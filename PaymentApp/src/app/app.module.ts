5225603cf4b093f5edefe4a5e46f0c2a04dcd663
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-11-29 20:50:49 +0530

    Added the project's files.

---
+++ PaymentApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentDetailFormComponent } from './Pages/payment-detail-form/payment-detail-form.component';
+import { PaymentDetailsComponent } from './Pages/payment-details/payment-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
