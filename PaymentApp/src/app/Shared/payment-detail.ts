b9a1384b5ef238419d7121594d819f6048cab6b9
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-12-05 20:23:56 +0530

    Added PaymentDetail class.

--- PaymentApp/src/app/Shared/payment-detail.ts
+++ PaymentApp/src/app/Shared/payment-detail.ts
@@ -1,2 +1,15 @@
 export class PaymentDetail {
+    paymentDetailId: number;
+    cardOwnerName: string;
+    cardNumber: string;
+    expirationDate: string;
+    securityCode: string;
+
+    constructor(paymentDetailId: number, cardOwnerName: string, cardNumber: string, expirationDate:string, securityCode: string) {
+        this.paymentDetailId = paymentDetailId;
+        this.cardOwnerName = cardOwnerName;
+        this.cardNumber = cardNumber;
+        this.expirationDate = expirationDate;
+        this.securityCode = securityCode;
+    }
 }
