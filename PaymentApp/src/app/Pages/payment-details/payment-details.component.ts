ec0c6f5eaa924acfa0fbbb66f054f5ecdd7e3db8
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-12-06 13:27:47 +0530

    Add the list of details in UI. Unsubscribe the observable via takeUntil pattern. Track the for loop by trackById().

--- PaymentApp/src/app/Pages/payment-details/payment-details.component.ts
+++ PaymentApp/src/app/Pages/payment-details/payment-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { PaymentDetail } from 'src/app/Shared/payment-detail';
 import { PaymentDetailService } from 'src/app/Shared/payment-detail.service';
 
@@ -7,17 +8,26 @@ import { PaymentDetailService } from 'src/app/Shared/payment-detail.service';
   templateUrl: './payment-details.component.html',
   styleUrls: ['./payment-details.component.scss']
 })
-export class PaymentDetailsComponent implements OnInit {
+export class PaymentDetailsComponent implements OnInit, OnDestroy {
   paymentDetails: PaymentDetail [] = [];
+  unsubscribe: Subject<void> = new Subject<void>();
 
   constructor(private _paymentDetails: PaymentDetailService) { }
 
   ngOnInit(): void {
-    this._paymentDetails.getPaymentDetails().subscribe((res:any) => {
+    this._paymentDetails.getPaymentDetails().pipe(takeUntil(this.unsubscribe)).subscribe((res:any) => {
         this.paymentDetails = res;
-        console.log('this.paymentDetails: ', this.paymentDetails);
+        // console.log('this.paymentDetails: ', this.paymentDetails);
       }
     );
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  trackById(item: any): number {
+    return item.id;
+  }
 }
