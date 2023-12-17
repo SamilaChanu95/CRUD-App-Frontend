131d852a0196dfb9546920644fbae63275629092
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-12-18 02:39:49 +0530

    Added delete method.

--- PaymentApp/src/app/Shared/payment-detail.service.ts
+++ PaymentApp/src/app/Shared/payment-detail.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
   // url: string = environment.apiBaseUrl + '/Payment/get-payment-list';
-  url: string = '/api/Payment/get-payment-list';
+  // url: string = '/api/Payment/get-payment-list';
 
   constructor(private _httpClient: HttpClient) { }
 
   public getPaymentDetails(): Observable<any> {
-    return this._httpClient.get(this.url);
+    return this._httpClient.get(`/api/Payment/get-payment-list`); 
   }
 
+  public deletePaymentDetail(id: number): Observable<any> {
+    return this._httpClient.delete(`/api/Payment/delete-payment/${id}`); 
+  }
 }
